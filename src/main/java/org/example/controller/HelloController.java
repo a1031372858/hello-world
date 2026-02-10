249618b1d15b9b99800dfaf05072fbb40ed62236
xuyachang <xuyachang@huitun.com>
2026-02-10 16:55:34 +0800

    去除Service层的接口，不存在多态情况

--- src/main/java/org/example/controller/HelloController.java
+++ src/main/java/org/example/controller/HelloController.java
@@ -3,15 +3,11 @@ package org.example.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.example.converter.UserConverter;
-import org.example.model.po.UserPO;
 import org.example.model.to.UserTO;
 import org.example.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Date;
 
 @Slf4j
 @RestController
--- src/main/java/org/example/service/UserService.java
+++ src/main/java/org/example/service/UserService.java
@@ -1,6 +1,7 @@
 package org.example.service;
 
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.converter.UserConverter;
 import org.example.model.po.UserPO;
@@ -9,8 +10,21 @@ import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
+@Slf4j
 @Service
-public interface UserService {
+@RequiredArgsConstructor
+public class UserService {
 
-     UserTO userInfo();
+    private final UserConverter userConverter;
+
+    public UserTO userInfo(){
+        UserPO userPO = new UserPO();
+        userPO.setId(1L);
+        userPO.setBirthday(new Date());
+        userPO.setName("张三");
+        userPO.setMobile("13000000000");
+        UserTO userTO = userConverter.po2to(userPO);
+        log.info(userTO.toString());
+        return userTO;
+    }
 }
